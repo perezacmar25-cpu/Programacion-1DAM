59edab8f0971fc932dc39e79613e823decb97c6f
perezacmar25-cpu <perez.acmar25@triana.salesianos.edu>
2026-04-07 14:24:40 +0200

    Ejercicio 01 avance version throw new

--- eclipse-workspace/EjerciciosT6/src/ejercicio01vthrows/Operaciones.java
+++ eclipse-workspace/EjerciciosT6/src/ejercicio01vthrows/Operaciones.java
@@ -14,4 +14,22 @@ public class Operaciones {
 		return num1 / num2;
 		
 	}
+	
+	public int sumar(int num1, int num2) {
+		
+		return num1 + num2;
+	}
+	
+	public int restar(int num1, int num2) {
+		
+		return num1 - num2;
+	}
+	
+	
+	public int multiplicar(int num1, int num2) {
+		
+		return num1 * num2;
+	}
+	
+	
 }
--- eclipse-workspace/EjerciciosT6/src/ejercicio01vthrows/Principal.java
+++ eclipse-workspace/EjerciciosT6/src/ejercicio01vthrows/Principal.java
@@ -1,24 +1,44 @@
 package ejercicio01vthrows;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		int op;
 		Operaciones o = new Operaciones();
-			
-			try {
-				o.dividir(2, 0);
-			}catch(ArithmeticException e) {
-				System.out.println(e.getMessage());
-			}catch(NumberFormatException e) {
-				System.out.println("Error");
+		op = Leer.datoInt();
+		try {
+			switch (op) {
+			case 1:
+					System.out.println("Diga un número");
+	                num1 = Leer.datoInt();
+	                System.out.println("Di otro número");
+	                num2 = Leer.datoInt();
+					o.sumar(2, 5);
+					break;
+			case 2:
+					o.dividir(2, 0);
+					break;
+				
+			case 3:
+					o.restar(op, op);
+					break;
+				
+			case 4:
+					o.multiplicar(op, op);
+					break;
 			}
-			catch(RuntimeException e) {
-				System.out.println(e.getMessage());
-			}
-		
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		} catch (NumberFormatException e) {
+			System.out.println("Error");
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
